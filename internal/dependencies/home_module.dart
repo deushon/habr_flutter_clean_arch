79c60e99197f3e438774a789aa1ebbc50760bf0d
deushon <30814931+deushon@users.noreply.github.com>
2024-03-07 04:29:52 +1000

    Delete internal directory

--- internal/dependencies/home_module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:front/domain/state/home/home_state.dart';
-import 'package:front/internal/dependencies/repository_module.dart';
-class HomeModule {
-  static HomeState homeState() {
-    return HomeState(
-      RepositoryModule.dayRepository(),
-    );
-  }
-}
