56d2f3136526ac1be210949a48ada5a011be3aa0
deushon <30814931+deushon@users.noreply.github.com>
2024-03-07 04:31:38 +1000

    Delete main.dart

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'internal/application.dart';
-import 'package:front/domain/state/home/home_state.dart';
-
-void main() {
-  runApp(Application());
-}
